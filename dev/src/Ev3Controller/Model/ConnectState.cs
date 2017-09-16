632fe8fea42b6631d2974d10c3eab3d4ca8de084
DeepSleepEngineer <deep-sleeper@nify.com>
2017-09-16 12:30:09 +0900

    Add some soure code for Solution. -Add ViewModelBase, DeviceViewModelBase, Ev3PortViewModel class. -Add Model class, ConnectState, ConnectStateChangedEventArgs, and IsConnectedChangedEventArgs class.

---
+++ dev/src/Ev3Controller/Model/ConnectState.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev3Controller.Model
+{
+    public enum ConnectionState
+    {
+        /// <summary>
+        /// Device is disconnected.
+        /// </summary>
+        Disconnected,
+        /// <summary>
+        /// Canceling the connection.
+        /// </summary>
+        Disconnecting,
+        /// <summary>
+        /// Establishing device connection.
+        /// </summary>
+        Connecting,
+        /// <summary>
+        /// Established device connection.
+        /// </summary>
+        Connected,
+        /// <summary>
+        /// Sending data to device.
+        /// </summary>
+        Sending,
+        /// <summary>
+        /// Receiving data from device.
+        /// </summary>
+        Receiving,
+        /// <summary>
+        /// Connection state with device is unknown.
+        /// </summary>
+        Unknown,
+    };
+
+    /// <summary>
+    /// Represents the connection state with device.
+    /// </summary>
+    public class ConnectState
+    {
+        #region Constructors and the Finalizer
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="State">Connection state to set to State property.</param>
+        public ConnectState(ConnectionState State)
+        {
+            this.State = State;
+        }
+        #endregion
+
+        #region Public properties
+        /// <summary>
+        /// Represent state of connection with device.
+        /// </summary>
+        public ConnectionState State { get; protected set; }
+        #endregion
+    }
+}
---
+++ dev/src/Ev3Controller/ViewModel/Ev3PortViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev3Controller.ViewModel
+{
+    using Model;
+
+    public class Ev3PortViewModel : DeviceViewModelBase
+    {
+        #region Constructors and the Finalizer
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Ev3PortViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Events
+        /// <summary>
+        /// Event to notify that the state of connection with device is changed.
+        /// </summary>
+        /// <param name="sender">Source object of event.</param>
+        /// <param name="e">Event argument contains state of connection when event raised.</param>
+        public delegate void ConnectStateChangedEventHandler(object sender, ConnectStateChangedEventArgs e);
+        public event ConnectStateChangedEventHandler ConnectStateChanged;
+        public virtual void OnConnectStateChanged(ConnectStateChangedEventArgs e)
+        {
+            this.ConnectStateChanged?.Invoke(this, e);
+        }
+        #endregion
+
+        #region Public properties
+        /// <summary>
+        /// State of connection.
+        /// </summary>
+        protected ConnectState _ConnectState;
+        public ConnectState ConnectState
+        {
+            get { return this._ConnectState; }
+            set
+            {
+                this._ConnectState = value;
+            }
+        }
+
+        /// <summary>
+        /// Name to show in label area presents current action.
+        /// </summary>
+        protected string _ActionName;
+        public string ActionName
+        {
+            get { return this.ActionName; }
+            set
+            {
+                this._ActionName = value;
+                this.RaisePropertyChanged("ActionName");
+            }
+        }
+
+        /// <summary>
+        /// Represents whether the port used to connect with device can be changed or not.
+        /// </summary>
+        protected bool _CanChangePort;
+        public bool CanChangePort
+        {
+            get { return this._CanChangePort; }
+            set
+            {
+                this._CanChangePort = value;
+                this.RaisePropertyChanged("CanChangePort");
+            }
+        }
+        #endregion
+
+        #region Other methods and private properties in calling order
+        /// <summary>
+        /// Callback method called when ConnectStateChanged event raised.
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Value to represent event data.</param>
+        public virtual void ConnectedStateChangedCallback(object sender, ConnectStateChangedEventArgs e)
+        {
+            //Write code here to handle ConnectStateChanged event.
+        }
+        #endregion
+    }
+}
