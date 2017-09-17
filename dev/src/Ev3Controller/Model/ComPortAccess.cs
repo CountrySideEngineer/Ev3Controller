42d4e78b206179bbaa2e8f78f9d33745ff9cf17f
DeepSleepEngineer <deep-sleeper@nify.com>
2017-09-17 18:18:38 +0900

    Add ComPortAccess class to access serial port. -Add ComPortAccess class to open and close, and read and write data. -Add unit test of ComPortAccess class.(Some method need accessible serial port.  !!Attention!!:In this environment, virtual COM port must be setup to run unit test.

---
+++ dev/src/Ev3Controller/Model/ComPortAccess.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev3Controller.Model
+{
+    /// <summary>
+    /// Provides interdace to access port.
+    /// This class refers to page below.
+    /// https://code.msdn.microsoft.com/windowsapps/COM-howto-6c7ff269
+    /// </summary>
+    public class ComPortAccess
+    {
+        #region Public constants
+        /// <summary>
+        /// Buffer si
+        /// </summary>
+        public const int DATA_BUF_SIZE = 32;
+        #endregion
+
+        #region Constructors and the Finalizer
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="ComPort"></param>
+        public ComPortAccess(ComPort ComPort)
+        {
+            this.ComPort = ComPort;
+        }
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        /// Represents a serial port resource.
+        /// </summary>
+        public SerialPort Port { get; protected set; }
+
+        /// <summary>
+        /// Represents a access port information.
+        /// </summary>
+        public ComPort ComPort { get; protected set; }
+        #endregion
+
+        #region Other methods and private properties in calling order
+
+        /// <summary>
+        /// Initialize send and receive buffer of serial port.
+        /// </summary>
+        public void Init()
+        {
+            if ((this.Port != null) && (this.Port.IsOpen))
+            {
+                this.Port.DiscardInBuffer();
+                this.Port.DiscardOutBuffer();
+            }
+            else
+            {
+                Console.WriteLine("Serial Port is not open.");
+            }
+        }
+
+        /// <summary>
+        /// Open serial port specified by argument ComPort.
+        /// The argument "ComPort is set to Property ComPort.
+        /// </summary>
+        /// <param name="ComPort">Serial port information to open.</param>
+        /// <returns>Returns true if the port can open, else returns false.</returns>
+        public bool Connect(ComPort ComPort)
+        {
+            this.ComPort = ComPort;
+
+            return this.Connect();
+        }
+        /// <summary>
+        /// Open serial port specified by property ComPort.
+        /// </summary>
+        /// <returns>Returns true if the port can open, else returns false.</returns>
+        public bool Connect()
+        {
+            try
+            {
+                /*
+                 * Prevent serial port from being opened duplicately and create new SerialPort
+                 * instance.
+                 */
+                if ((this.Port == null) || (!this.Port.IsOpen))
+                {
+                    this.Port = new SerialPort(this.ComPort.Name,
+                        this.ComPort.BaudrateValue,
+                        this.ComPort.Parity,
+                        this.ComPort.DataBit,
+                        this.ComPort.StopBits);
+                    this.Port.Open();
+                    this.Port.ReadTimeout = 2000;//2.0 sec time out.
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine("Catch UnauthorizedAccessException");
+                Console.WriteLine("[ERROR]{0}", ex.StackTrace);
+
+                return false;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Catch ArgumentOutOfRangeException");
+                Console.WriteLine("[ERROR]{0}", ex.StackTrace);
+
+                return false;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Catch ArgumentException");
+                Console.WriteLine("[ERROR]{0}", ex.StackTrace);
+
+                return false;
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Catch IOException");
+                Console.WriteLine("[ERROR]{0}", ex.StackTrace);
+
+                return false;
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("Catch InvalidOperationException");
+                Console.WriteLine("[ERROR]{0}", ex.StackTrace);
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Close serial prot specified by ComPort and Port property.
+        /// </summary>
+        public void Disconnect()
+        {
+            try
+            {
+                if ((this.Port != null) && (this.Port.IsOpen))
+                {
+                    this.Port.Close();
+                    this.Port = null;
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Catch InvalidOperationException");
+                Console.WriteLine("[ERROR]{0}", ex.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// Send data through serial port.
+        /// </summary>
+        /// <param name="Data">Byte data to send.</param>
+        /// <returns>Send data size.</returns>
+        public virtual int SendData(byte[] Data)
+        {
+            int data_size = 0;
+            if ((this.ComPort != null) && (this.Port.IsOpen))
+            {
+                this.Port.Write(Data, 0, Data.Length);
+                data_size = Data.Length;
+                //To fullfil command data buffer on EV3 side, send data till the buffer is filled up.
+                for (int index = Data.Length; index < (int)DATA_BUF_SIZE; index++)
+                {
+                    char[] Padding = { (char)0x00 };
+                    this.Port.Write(Padding, 0, 1);
+                    data_size++;
+                }
+            }
+            return data_size;
+        }
+
+        /// <summary>
+        /// Read data from serial port.
+        /// </summary>
+        /// <param name="Data">Reference to buffer to store read data.</param>
+        /// <returns>Size of read data.</returns>
+        public virtual int RecvData(byte[] Data)
+        {
+            int LengthRead = 0;
+            if ((this.Port != null) && (this.Port.IsOpen))
+            {
+                do
+                {
+                    int LengthToRead = this.Port.BytesToRead;
+                    Data = new byte[LengthToRead];
+
+                    while (LengthRead < LengthToRead)
+                    {
+                        int ReadLen = this.Port.Read(Data, LengthRead, LengthToRead - LengthRead);
+                        LengthRead += ReadLen;
+                    }
+                    if (LengthRead > 0)
+                    {
+                        break;
+                    }
+                } while (this.Port.IsOpen);
+            }
+
+            return LengthRead;
+        }
+        #endregion
+    }
+}
---
+++ dev/src/Ev3ControllerTests/Model/ComPortAccessTests.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ev3Controller.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using Ev3Controller.ViewModel;
+
+namespace Ev3Controller.Model.Tests
+{
+    [TestClass()]
+    public class ComPortAccessTests
+    {
+        public ComPortAccess TestTarget;
+
+        [TestInitialize()]
+        public void InitTest()
+        {
+            this.TestTarget = new ComPortAccess(new ComPort("COM44", "Device"));
+        }
+
+        [TestCleanup()]
+        public void FinalizeTest()
+        {
+            this.TestTarget.Disconnect();
+        }
+
+        #region Unit test of Connect method.
+        [TestMethod()]
+        [TestCategory("ComPortAccess_Connect")]
+        public void ConnectTest_001()
+        {
+            Assert.IsTrue(this.TestTarget.Connect());
+            Assert.IsTrue(this.TestTarget.Port.IsOpen);
+        }
+        [TestMethod()]
+        [TestCategory("ComPortAccess_Connect")]
+        public void ConnectTest_002()
+        {
+            this.TestTarget = new ComPortAccess(new ComPort("COM100", "Device"));
+            Assert.IsFalse(this.TestTarget.Connect());
+            Assert.IsFalse(this.TestTarget.Port.IsOpen);
+        }
+        [TestMethod()]
+        [TestCategory("ComPortAccess_Connect")]
+        public void ConnectTest_003()
+        {
+            this.TestTarget = new ComPortAccess(new ComPort("COM10", "Device"));
+            Assert.IsFalse(this.TestTarget.Connect());
+            Assert.IsFalse(this.TestTarget.Port.IsOpen);
+        }
+        [TestMethod()]
+        [TestCategory("ComPortAccess_Connect")]
+        public void ConnectTest_004()
+        {
+            Assert.IsTrue(this.TestTarget.Connect(new ComPort("COM45", "Device2")));
+            Assert.IsTrue(this.TestTarget.Port.IsOpen);
+        }
+        [TestMethod()]
+        [TestCategory("ComPortAccess_Connect")]
+        public void ConnectTest_005()
+        {
+            Assert.IsTrue(this.TestTarget.Connect());
+            Assert.IsFalse(this.TestTarget.Connect());
+            Assert.IsTrue(this.TestTarget.Port.IsOpen);
+        }
+        #endregion
+
+        #region Unit test of Disconnect method.
+        [TestMethod()]
+        [TestCategory("ComPortAccess_Disconnect")]
+        public void DisconnectTest_001()
+        {
+            this.TestTarget.Connect();
+            this.TestTarget.Disconnect();
+            Assert.IsNull(this.TestTarget.Port);
+        }
+        [TestMethod()]
+        [TestCategory("ComPortAccess_Disconnect")]
+        public void DisconnectTest_002()
+        {
+            this.TestTarget.Connect();
+            this.TestTarget.Disconnect();
+            this.TestTarget.Disconnect();
+        }
+        #endregion
+
+        #region Unit test of SendData method.
+        [TestMethod()]
+        [TestCategory("ComPortAccess_SendData")]
+        public void SendDataTest_001()
+        {
+            byte[] Data = new byte[18];
+            for (int i = 0; i < 18; i++)
+            {
+                Data[i] = (byte)(97 + i);//97 means 'a' in ASCII code.
+            }
+
+            this.TestTarget.Connect();
+            this.TestTarget.SendData(Data);
+            this.TestTarget.Disconnect();
+        }
+        #endregion
+
+        [TestMethod()]
+        [TestCategory("ComPortAccess_Init")]
+        public void InitTest1()
+        {
+            this.TestTarget.Connect();
+            this.TestTarget.Init();
+
+            Assert.AreEqual(this.TestTarget.Port.BytesToRead, 0);
+            Assert.AreEqual(this.TestTarget.Port.BytesToWrite, 0);
+        }
+    }
+}
