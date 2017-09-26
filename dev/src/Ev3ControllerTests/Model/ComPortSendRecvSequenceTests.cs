4a08053e508ff1ee3d4ba4ba03d7c4190ee5edb2
DeepSleepEngineer <deep-sleeper@nify.com>
2017-09-27 00:01:37 +0900

    Add base class for unit test of ComPortAccessSequence class.

--- dev/src/Ev3ControllerTests/Model/ComPortSendRecvSequenceTests.cs
+++ dev/src/Ev3ControllerTests/Model/ComPortSendRecvSequenceTests.cs
@@ -6,35 +6,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using Ev3ControllerTests.Model.Tests;
 
 namespace Ev3Controller.Model.Tests
 {
     [TestClass()]
-    public class ComPortSendRecvSequenceTests
+    public class ComPortSendRecvSequenceTests : ComPortAccessSequenceTestBase
     {
-        public void Init()
-        {
-            this.IsSequenceStartedEventHandler = false;
-            this.IsSequenceStartingEventHandler = false;
-            this.IsSequenceFinishedEventHandler = false;
-            this.IsSequenceStartingEventHandlerCount = 0;
-            this.IsSequenceStartedEventHandlerCount = 0;
-            this.IsSequenceFinishedEventHandlerCount = 0;
-        }
-
         [TestMethod()]
         [TestCategory("ComPortSendRecvSequence_Sequence")]
         public void SequenceTest_001()
         {
             this.Init();
-            ComPortAccessSequenceRunner Runner =
-                new ComPortAccessSequenceRunner(new ComPort("COM41", "Device"));
 
-            Runner.SequenceStartedEvent += this.OnSequenceStartedEventHandler;
-            Runner.SequenceStartingEvent += this.OnSequenceStartingEventHandler;
-            Runner.SequenceFinishedEvent += this.OnSequenceFinishedEventHandler;
-
-            this.Init();
             Runner.ChangeSequence(ComPortAccessSequenceRunner.SequenceName.SEQUENCE_NAME_CONNECT);
             while (!Runner.CurTask.Status.Equals(TaskStatus.RanToCompletion)) ;
 
@@ -69,32 +53,6 @@ namespace Ev3Controller.Model.Tests
             Assert.AreEqual(this.IsSequenceStartedEventHandlerCount, 1);
             //1st:Send and received senqeuce stopped, 2nd:Stop sequence finished.
             Assert.AreEqual(this.IsSequenceFinishedEventHandlerCount, 2);
-
-            Runner.SequenceStartedEvent -= this.OnSequenceStartedEventHandler;
-            Runner.SequenceStartingEvent -= this.OnSequenceStartingEventHandler;
-            Runner.SequenceFinishedEvent -= this.OnSequenceFinishedEventHandler;
-        }
-
-        protected bool IsSequenceStartingEventHandler;
-        protected int IsSequenceStartingEventHandlerCount;
-        public void OnSequenceStartingEventHandler(object sender, EventArgs e)
-        {
-            IsSequenceStartingEventHandler = true;
-            IsSequenceStartingEventHandlerCount++;
-        }
-        protected bool IsSequenceStartedEventHandler;
-        protected int IsSequenceStartedEventHandlerCount;
-        public void OnSequenceStartedEventHandler(object sender, EventArgs e)
-        {
-            IsSequenceStartedEventHandler = true;
-            IsSequenceStartedEventHandlerCount++;
-        }
-        protected bool IsSequenceFinishedEventHandler;
-        protected int IsSequenceFinishedEventHandlerCount;
-        public void OnSequenceFinishedEventHandler(object sender, EventArgs e)
-        {
-            IsSequenceFinishedEventHandler = true;
-            IsSequenceFinishedEventHandlerCount++;
         }
     }
 }
