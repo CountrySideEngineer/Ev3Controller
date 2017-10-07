7d8aa21e10ebb03124c8194dedb1846f71f4fcfb
DeepSleepEngineer <deep-sleeper@nify.com>
2017-10-07 16:49:21 +0900

    Add Command_0E_20 class. -Add Command_0E_20 class source code, getting gyro sensor angle position. -Add unit test of Command_0E_20 class.

---
+++ dev/src/Ev3Controller/Ev3Command/Command_0E_20.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev3Controller.Ev3Command
+{
+    public class Command_0E_20 : Command_0E
+    {
+        #region Other methods and private properties in calling order
+        protected override void Init()
+        {
+            this.SubCmd = 0x20;
+            this.CmdLen = 0x00;
+
+            this.SubRes = 0x20;
+            this.ResLen = 0xFF;
+
+            this.OneDataLen = 0x03;
+
+            base.Init();
+        }
+
+        /// <summary>
+        /// Setup command data for GetMotors original data.
+        /// </summary>
+        protected override void SetUp()
+        {
+            this.CmdData[(int)COMMAND_BUFF_INDEX.COMMAND_BUFF_INDEX_CMD_DATA_LEN] = this.CmdLen;
+        }
+
+        /// <summary>
+        /// Check reponse data of GetSensors and get ultrasonic sensor listening data.
+        /// </summary>
+        protected override void CheckParam()
+        {
+            base.CheckParam();
+            this.CheckLenAndThrowException();
+
+            int DevNum = this.ResData[(int)RESPONSE_BUFF_INDEX.RESPONSE_BUFF_INDEX_RES_DATA_TOP];
+            if (DevNum != 0)
+            {
+                int PortIndex = (int)RESPONSE_BUFF_INDEX.RESPONSE_BUFF_INDEX_RES_DATA_TOP + 1;
+                for (int index = 0; index < DevNum; index++)
+                {
+                    this.CheckPortAndThrowException(PortIndex);
+                    PortIndex += 3;
+                }
+            }
+            base.CheckParam();
+        }
+        #endregion
+    }
+}
