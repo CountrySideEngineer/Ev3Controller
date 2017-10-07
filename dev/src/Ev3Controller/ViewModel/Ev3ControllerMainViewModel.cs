f3a276ee4830772dad88d18fcf44918b7d76bb5f
DeepSleepEngineer <deep-sleeper@nify.com>
2017-10-07 17:03:44 +0900

    Add Ev3ControllerMainViewModel class. -Add Ev3ControllerMainViewModel class, no code written source file.

---
+++ dev/src/Ev3Controller/ViewModel/Ev3ControllerMainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev3Controller.ViewModel
+{
+    public class Ev3ControllerMainViewModel : ViewModelBase
+    {
+    }
+}
