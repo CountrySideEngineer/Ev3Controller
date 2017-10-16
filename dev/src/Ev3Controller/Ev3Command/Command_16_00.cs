cbd95bfed484df29ccfc71fe0eb2e8b80d0083f1
DeepSleepEngineer <deep-sleeper@nify.com>
2017-10-16 22:14:38 +0900

    Run unit test of CommandParam_16_00 and Command_16_00 class. -Add unit test of CommandParam_16_00 and Command_16_00 class. -Modify bugs found in unit test.

---
+++ dev/src/Ev3Controller/Ev3Command/Command_16_00.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev3Controller.Ev3Command
+{
+    public class Command_16_00 : ACommand
+    {
+        #region Constructors and the Finalizer
+        public Command_16_00() : this(new CommandParam_16_00(0)) { }
+        public Command_16_00(ICommandParam CommandParam) : base(CommandParam)
+        {
+            try
+            {
+                if (CommandParam is CommandParam_16_00)
+                {
+                    this.CommandParam = CommandParam as CommandParam_16_00;
+                }
+                else
+                {
+                    this.CommandParam = new CommandParam_16_00(0);
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+                this.CommandParam = new CommandParam_16_00(0);
+            }
+        }
+        #endregion
+
+        #region Other methods and private properties in calling order
+        /// <summary>
+        /// Initialize command parameters.
+        /// </summary>
+        protected override void Init()
+        {
+            this.Name = "SetSteer";
+            this.Cmd = 0x16;
+            this.SubCmd = 0x00;
+            this.CmdLen = 0x01;
+
+            this.Res = 0x17;
+            this.SubRes = 0x00;
+            this.ResLen = 0x01;
+
+            base.Init();
+        }
+
+        /// <summary>
+        /// Setup command data consisted of "byte" data array.
+        /// </summary>
+        protected override void SetUp(ICommandParam CommandParam)
+        {
+            try
+            {
+                int DataIndex = (int)COMMAND_BUFF_INDEX.COMMAND_BUFF_INDEX_CMD_DATA_LEN;
+                this.CmdData[DataIndex++] = this.CmdLen;
+                this.CmdData[DataIndex] = (byte)((CommandParam as CommandParam_16_00).Steer);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                int DataIndex = (int)COMMAND_BUFF_INDEX.COMMAND_BUFF_INDEX_CMD_DATA_LEN;
+                this.CmdData[DataIndex++] = this.CmdLen;
+                this.CmdData[DataIndex] = 0;
+            }
+        }
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        /// Object contains parameter to be set in command data array.
+        /// </summary>
+        public ICommandParam CommandParam { get; protected set; }
+        #endregion
+    }
+}
---
+++ dev/src/Ev3ControllerTests/Ev3Command/CommandParam_16_00_Tests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ev3Controller.Ev3Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev3Controller.Ev3Command.Tests
+{
+    [TestClass()]
+    public class CommandParam_16_00_Tests
+    {
+        #region Unit test of CommandParam_16_00
+        [TestMethod()]
+        [TestCategory("Constructor")]
+        [TestCategory("CommandParam_16_00")]
+        [TestCategory("CommandParam_16_00_Constructor")]
+        public void CommandParam_16_00_Test_001()
+        {
+            var CommandParam = new CommandParam_16_00(0);
+
+            Assert.AreEqual(0, CommandParam.Steer);
+        }
+        [TestMethod()]
+        [TestCategory("Constructor")]
+        [TestCategory("CommandParam_16_00")]
+        [TestCategory("CommandParam_16_00_Constructor")]
+        public void CommandParam_16_00_Test_002()
+        {
+            var CommandParam = new CommandParam_16_00(1);
+
+            Assert.AreEqual(1, CommandParam.Steer);
+        }
+        [TestMethod()]
+        [TestCategory("Constructor")]
+        [TestCategory("CommandParam_16_00")]
+        [TestCategory("CommandParam_16_00_Constructor")]
+        public void CommandParam_16_00_Test_003()
+        {
+            var CommandParam = new CommandParam_16_00(-1);
+
+            Assert.AreEqual(-1, CommandParam.Steer);
+        }
+        #endregion
+    }
+}
