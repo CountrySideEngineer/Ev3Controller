2b49bc50c7add51f8d120a23c1d7bb11f2007d4d
DeepSleepEngineer <deep-sleeper@nify.com>
2017-10-19 21:11:41 +0900

    Add "Command_50" series class, "GetGyroSensor" command. -Add "GetGyroSensor" command classes. -Add and run their unit test.

---
+++ dev/src/Ev3ControllerTests/Ev3Command/Command_50_00_Tests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ev3Controller.Ev3Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev3Controller.Ev3Command.Tests
+{
+    [TestClass()]
+    public class Command_50_00_Tests
+    {
+        #region Unit test of Command_50_00 Constructor.
+        [TestMethod()]
+        [TestCategory("Command_50_00")]
+        public void Command_50_00_Test()
+        {
+            var Command = new Command_50_00();
+
+            Assert.AreEqual("GetGyroSensor", Command.Name);
+            Assert.AreEqual(0x50, Command.Cmd);
+            Assert.AreEqual(0x00, Command.SubCmd);
+            Assert.AreEqual(0x00, Command.CmdLen);
+            Assert.AreEqual(0x50, Command.CmdData[0]);
+            Assert.AreEqual(0x00, Command.CmdData[1]);
+            Assert.AreEqual(0x00, Command.CmdData[2]);
+            Assert.AreEqual(0x03, Command.OneDataLen);
+            Assert.AreEqual(0x00, Command.SubRes);
+            Assert.AreEqual(0xFF, Command.ResLen);
+        }
+        #endregion
+    }
+}
