d0cd77d872f98c49664d1a68824630a4f8c7c60f
DeepSleepEngineer <deep-sleeper@nify.com>
2017-10-21 10:10:45 +0900

    Add Ev3 device class. -Add Ev3MotorDevice class. -Add Ev3SensorDevice class. -Add Ev3Device class, super class of Ev3MotorDevice and Ev3SensorDevice.

---
+++ dev/src/Ev3Controller/Model/Ev3Device.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev3Controller.Model
+{
+    public class Ev3Device
+    {
+        #region Constructors and the Finalizer
+        public Ev3Device()
+        {
+            this.Port = "";
+            this.Name = "";
+            this.Device = "";
+        }
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        /// Name of port.
+        /// </summary>
+        public string Port { get; set; }
+
+        /// <summary>
+        /// Device name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Device type.
+        /// </summary>
+        public string Device { get; set; }
+        #endregion
+    }
+}
