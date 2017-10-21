361eabcb4e0a770ab7b4f41b4ff518bbe62efe22
DeepSleepEngineer <deep-sleeper@nify.com>
2017-10-22 07:12:35 +0900

    Update Ev3Device and derived from it. -Add port enumerator, INPORT and OUTPORT, and port name dictionaries. -Change Port and Device properties. -Add device type enumerator and dictionary to Ev3MotorDevice and Ev3SensorDevice.

--- dev/src/Ev3Controller/Model/Ev3MotorDevice.cs
+++ dev/src/Ev3Controller/Model/Ev3MotorDevice.cs
@@ -8,9 +8,39 @@ namespace Ev3Controller.Model
 {
     public class Ev3MotorDevice : Ev3Device
     {
+        #region Public constants
+        /// <summary>
+        /// Enumrator of motor device type.
+        /// </summary>
+        public enum DEVICE_TYPE
+        {
+            MOTOR_DEVICE_NO_DEVICE,
+            MOTOR_DEVICE_MEDIUM_MOTOR,
+            MOTOR_DEVICE_LARGE_MOTOR,
+            MOTOR_DEVICE_UNADJUSTED,
+            MOTOR_DEVICE_UNKNOWN,
+            MOTOR_DEVICE_MAX,
+        }
+        #endregion
+
+        #region Public read-only static fields
+        public static readonly Dictionary<DEVICE_TYPE, string>
+            DeviceTypeDictionary = new Dictionary<DEVICE_TYPE, string>()
+        {
+                { DEVICE_TYPE.MOTOR_DEVICE_NO_DEVICE, "NO DEVICE" },
+                { DEVICE_TYPE.MOTOR_DEVICE_LARGE_MOTOR, "LARGE MOTOR" },
+                { DEVICE_TYPE.MOTOR_DEVICE_MEDIUM_MOTOR, "MEDIUM MOTOR" },
+                { DEVICE_TYPE.MOTOR_DEVICE_UNADJUSTED, "UnAdjusted" },
+                { DEVICE_TYPE.MOTOR_DEVICE_UNKNOWN, "Unknown" },
+                { DEVICE_TYPE.MOTOR_DEVICE_MAX, "Unknown" },
+        };
+        #endregion
+
         #region Constructors and the Finalizer
         public Ev3MotorDevice()
         {
+            this.ConnectedPort = OUTPORT.OUTPORT_MAX;
+            this.DeviceType = DEVICE_TYPE.MOTOR_DEVICE_NO_DEVICE;
             this.Power = 0;
             this.Counts = 0;
         }
@@ -26,6 +56,36 @@ namespace Ev3Controller.Model
         /// Angle position of motor.
         /// </summary>
         public long Counts { get; set; }
+
+        /// <summary>
+        /// Port the motor device is connected.
+        /// </summary>
+        public OUTPORT ConnectedPort;
+
+        /// <summary>
+        /// Motor device type.
+        /// </summary>
+        public DEVICE_TYPE DeviceType;
+        #endregion
+
+        #region Other methods and private properties in calling order
+        /// <summary>
+        /// Abstract method which returns port name the device is connected.
+        /// </summary>
+        /// <returns></returns>
+        public override string GetPortName()
+        {
+            return OutPortNameDictionary[this.ConnectedPort];
+        }
+
+        /// <summary>
+        /// Abstract method which returns connected device name.
+        /// </summary>
+        /// <returns></returns>
+        public override string GetDeviceName()
+        {
+            return DeviceTypeDictionary[this.DeviceType];
+        }
         #endregion
     }
 }
