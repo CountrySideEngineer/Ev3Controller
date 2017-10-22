b19f101e6e7c99840ba7ae6d1bfe78af7f8334c9
DeepSleepEngineer <deep-sleeper@nify.com>
2017-10-22 23:39:13 +0900

    Add BrickUpdater_20_01 class, to handle GetSonicSensor command. -Add BrickUpdater_20_01 class to handle GetSonicSensor command which to set listening state read from ultra sonic sensor and connection, connected or not, of each motors.

---
+++ dev/src/Ev3Controller/Model/BrickUpdater_20_01.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ev3Controller.Ev3Command;
+using System.Diagnostics;
+
+namespace Ev3Controller.Model
+{
+    public class BrickUpdater_20_01 : BrickUpdater
+    {
+        /// <summary>
+        /// Update sensor data, especially ultra sonic senosr listening data, device information
+        /// of Ev3 Brick sent by GetSonicSensor command.
+        /// </summary>
+        /// <param name="Command">GetSonicSensor command data.</param>
+        /// <param name="Brick">Ev3Brick object to set received data.</param>
+        public override void Update(ACommand Command, Ev3Brick Brick)
+        {
+            Debug.Assert(Command != null);
+            Debug.Assert(Brick != null);
+
+            if (Command is Command_20_01)
+            {
+                int Index = 0;
+                int DataTopIndex = 4;
+                int DevNum = Command.ResData[DataTopIndex++];
+                for (Index = 0; Index < DevNum; Index++)
+                {
+                    int DataIndex = DataTopIndex + (Index * 2);
+                    byte Port = Command.ResData[DataIndex++];
+                    byte IsListen = Command.ResData[DataIndex];
+                    Brick.SensorDevice(Port).ConnectedPort = (Ev3Device.INPORT)Port;
+                    Brick.SensorDevice(Port).IsConnected = true;
+                    Brick.SensorDevice(Port).Value2 = IsListen;
+                }
+            }
+        }
+    }
+}
