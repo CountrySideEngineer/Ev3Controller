3cb1c1acbea19a710f7f7c951ad5c1fed8889eb0
DeepSleepEngineer <deep-sleeper@nify.com>
2017-10-23 21:44:55 +0900

    Add BrickUpdater_30_00, BrickUpdater_30_01, and BrickUpdater_30_02 class, handle GetColorSensor command.

---
+++ dev/src/Ev3Controller/Model/BrickUpdater_30_00.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ev3Controller.Ev3Command;
+using System.Diagnostics;
+
+namespace Ev3Controller.Model
+{
+    public class BrickUpdater_30_00 : BrickUpdater
+    {
+        /// <summary>
+        /// Update sensor data, especially color sensor color ambient light strength, device
+        /// information of Ev3 Brick sent by GetColorSensor command.
+        /// </summary>
+        /// <param name="Command">GetColorSensor command data.</param>
+        /// <param name="Brick">Ev3Brick object to set received data.</param>
+        public override void Update(ACommand Command, Ev3Brick Brick)
+        {
+            Debug.Assert(Command != null);
+            Debug.Assert(Brick != null);
+
+            if (Command is Command_30_00)
+            {
+                int Index = 0;
+                int DataTopIndex = 4;
+                int DevNum = Command.ResData[DataTopIndex++];
+                for (Index = 0; Index < DevNum; Index++)
+                {
+                    int DataIndex = DataTopIndex + (Index * 2);
+                    byte Port = Command.ResData[DataIndex++];
+                    byte Ambient = Command.ResData[DataIndex];
+                    Brick.SensorDevice(Port).ConnectedPort = (Ev3Device.INPORT)Port;
+                    Brick.SensorDevice(Port).IsConnected = true;
+                    Brick.SensorDevice(Port).Value1 = Ambient;
+                }
+            }
+        }
+    }
+}
---
+++ dev/src/Ev3Controller/Model/BrickUpdater_30_01.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ev3Controller.Ev3Command;
+using System.Diagnostics;
+
+namespace Ev3Controller.Model
+{
+    public class BrickUpdater_30_01 : BrickUpdater
+    {
+        /// <summary>
+        /// Updater sensor data, especially color sensor refrection data, device information
+        /// of Ev3Brick sent by GetColorSensor command.
+        /// </summary>
+        /// <param name="Command">GetColorSensor command data.</param>
+        /// <param name="Brick">Ev3Brick object to set received data.</param>
+        public override void Update(ACommand Command, Ev3Brick Brick)
+        {
+            Debug.Assert(Command != null);
+            Debug.Assert(Brick != null);
+
+            if (Command is Command_30_01)
+            {
+                int Index = 0;
+                int DataTopIndex = 4;
+                int DevNum = Command.ResData[DataTopIndex++];
+                for (Index = 0; Index < DevNum; Index++)
+                {
+                    int DataIndex = DataTopIndex + (DevNum * 2);
+                    byte Port = Command.ResData[DataIndex++];
+                    byte Color = Command.ResData[DataIndex];
+                    Brick.SensorDevice(Port).ConnectedPort = (Ev3SensorDevice.INPORT)Port;
+                    Brick.SensorDevice(Port).IsConnected = true;
+                    Brick.SensorDevice(Port).Value2 = Color;
+                }
+            }
+        }
+    }
+}
