411786c19f296c16122b2ca400febe0ecbe97d77
DeepSleepEngineer <deep-sleeper@nify.com>
2017-10-26 06:04:54 +0900

    Add NotifyConnectException class. -Add NotifyConnectException class which notifies exception raised while connecting or disconneting.

---
+++ dev/src/Ev3Controller/Model/NotifyConnectException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev3Controller.Model
+{
+    class NotifyConnectExceptionEventArgs : EventArgs
+    {
+        #region Constructors and the Finalizer
+        /// <summary>
+        /// Constructor of NotifyConnectExceptionEventArgs.
+        /// </summary>
+        /// <param name="Exp">Exception data to notify.</param>
+        public NotifyConnectExceptionEventArgs(Exception Exp)
+        {
+            this.Except = Exp;
+        }
+        #endregion
+
+        #region Public properties
+        /// <summary>
+        /// Exception containts information about command.
+        /// </summary>
+        public Exception Except { get; protected set; }
+        #endregion
+    }
+}
