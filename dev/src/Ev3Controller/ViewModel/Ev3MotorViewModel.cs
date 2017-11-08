884fbbfe34f5b0a40259fbd9444658929ef4243b
DeepSleepEngineer <deep-sleeper@nify.com>
2017-11-09 00:06:36 +0900

    Add view model of motor. -Add Ev3MotorViewModel class, ViewModel of Motor. -Add base class of device, motor and sensor, PortConnectDeviceViewModelBase.

---
+++ dev/src/Ev3Controller/ViewModel/Ev3MotorViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev3Controller.ViewModel
+{
+    public class Ev3MotorViewModel : PortConnectDeviceViewModelBase
+    {
+        #region Constructors and the Finalizer
+        public Ev3MotorViewModel() { }
+        #endregion
+
+        #region Public Properties
+        /// <summary>Target motor output power.</summary>
+        protected int _TargetOutput;
+        public int TargetOutput
+        {
+            get { return this._TargetOutput; }
+            set
+            {
+                this._TargetOutput = value;
+                this.RaisePropertyChanged("TargetOutput");
+            }
+        }
+
+        /// <summary>Unit of target motor output power.</summary>
+        protected string _TargetOutputUnit;
+        public string TargetOutputUnit
+        {
+            get { return this._TargetOutputUnit; }
+            set
+            {
+                this._TargetOutputUnit = value;
+                this.RaisePropertyChanged("TargetOutputUnit");
+            }
+        }
+
+        /// <summary>Current motor output power.</summary>
+        protected int _CurrentOutput;
+        public int CurrentOutput
+        {
+            get { return this._CurrentOutput; }
+            set
+            {
+                this._CurrentOutput = value;
+                this.RaisePropertyChanged("CurrentOutput");
+            }
+        }
+
+        /// <summary>Unit of current motor output power.</summary>
+        protected string _CurrentOutputUnit;
+        protected string CurrentOutputUnit
+        {
+            get { return this._CurrentOutputUnit; }
+            set
+            {
+                this._CurrentOutputUnit = value;
+                this.RaisePropertyChanged("CurrentOutputUnit");
+            }
+        }
+        #endregion
+    }
+}
---
+++ dev/src/Ev3Controller/ViewModel/PortConnectDeviceViewModelBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev3Controller.ViewModel
+{
+    public class PortConnectDeviceViewModelBase : DeviceViewModelBase
+    {
+        #region Constructors and the Finalizer
+        public PortConnectDeviceViewModelBase() { }
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        /// Name of port the device connected to.
+        /// </summary>
+        protected string _PortName;
+        public string PortName
+        {
+            get { return this._PortName; }
+            set
+            {
+                this._PortName = value;
+                this.RaisePropertyChanged("PortName");
+            }
+        }
+
+        /// <summary>
+        /// Name of device connected to a port.
+        /// </summary>
+        protected string _DeviceName;
+        public string DeviceName
+        {
+            get { return this._DeviceName; }
+            set
+            {
+                this._DeviceName = value;
+                this.RaisePropertyChanged("DeviceName");
+            }
+        }
+        #endregion
+    }
+}
