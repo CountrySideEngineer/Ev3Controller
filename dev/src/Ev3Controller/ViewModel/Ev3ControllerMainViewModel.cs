b7c887c96f450204e12b2ea5412de1544445bbde
DeepSleepEngineer <deep-sleeper@nify.com>
2017-11-11 07:06:25 +0900

    Add device view model array. -Add array of device view model, MotorDeviceArray and SensorViewModelArray. -Change style to access each device view model from protected field to by index of array.

--- dev/src/Ev3Controller/ViewModel/Ev3ControllerMainViewModel.cs
+++ dev/src/Ev3Controller/ViewModel/Ev3ControllerMainViewModel.cs
@@ -6,13 +6,22 @@ using System.Threading.Tasks;
 
 namespace Ev3Controller.ViewModel
 {
+    using Ev3Controller.Model;
+    using System.Timers;
+
     public class Ev3ControllerMainViewModel : ViewModelBase
     {
         #region Constructors and the Finalizer
         public Ev3ControllerMainViewModel()
         {
+            for (int index = 0; index < 4; index++)
+            {
+                this.MotorViewModelArray[index] = new Ev3MotorDeviceViewModel();
+                this.SensorViewModelArray[index] = new Ev3SensorDeviceViewModel();
+            }
 
             this.PortViewModel.ConnectStateChanged += this.ConnectStateChangedCallback;
+            this.UpdateTimer = null;
         }
         #endregion
 
@@ -41,20 +50,19 @@ namespace Ev3Controller.ViewModel
         /// <summary>
         /// View model object about motor device connected port A of Brick.
         /// </summary>
-        protected Ev3MotorDeviceViewModel _MotorViewModelA;
         public Ev3MotorDeviceViewModel MotorViewModelA
         {
             get
             {
-                if (null == this._MotorViewModelA)
+                if (null == this._MotorViewModelArray[0])
                 {
-                    this._MotorViewModelA = new Ev3MotorDeviceViewModel();
+                    this._MotorViewModelArray[0] = new Ev3MotorDeviceViewModel();
                 }
-                return this._MotorViewModelA;
+                return this._MotorViewModelArray[0];
             }
             set
             {
-                this._MotorViewModelA = value;
+                this._MotorViewModelArray[0] = value;
                 this.RaisePropertyChanged("MotorViewModelA");
             }
         }
@@ -62,20 +70,19 @@ namespace Ev3Controller.ViewModel
         /// <summary>
         /// View model object about motor device connected port B of Brick.
         /// </summary>
-        protected Ev3MotorDeviceViewModel _MotorViewModelB;
         public Ev3MotorDeviceViewModel MotorViewModelB
         {
             get
             {
-                if (null == this._MotorViewModelB)
+                if (null == this._MotorViewModelArray[1])
                 {
-                    this._MotorViewModelB = new Ev3MotorDeviceViewModel();
+                    this._MotorViewModelArray[1] = new Ev3MotorDeviceViewModel();
                 }
-                return this._MotorViewModelB;
+                return this._MotorViewModelArray[1];
             }
             set
             {
-                this._MotorViewModelB = value;
+                this._MotorViewModelArray[1] = value;
                 this.RaisePropertyChanged("MotorViewModelB");
             }
         }
@@ -83,20 +90,19 @@ namespace Ev3Controller.ViewModel
         /// <summary>
         /// View model object about motor device connected port C of Brick.
         /// </summary>
-        protected Ev3MotorDeviceViewModel _MotorViewModelC;
         public Ev3MotorDeviceViewModel MotorViewModelC
         {
             get
             {
-                if (null == this._MotorViewModelC)
+                if (null == this._MotorViewModelArray[2])
                 {
-                    this._MotorViewModelC = new Ev3MotorDeviceViewModel();
+                    this._MotorViewModelArray[2] = new Ev3MotorDeviceViewModel();
                 }
-                return this._MotorViewModelC;
+                return this._MotorViewModelArray[2];
             }
             set
             {
-                this._MotorViewModelC = value;
+                this._MotorViewModelArray[2] = value;
                 this.RaisePropertyChanged("MotorViewModelC");
             }
         }
@@ -104,41 +110,55 @@ namespace Ev3Controller.ViewModel
         /// <summary>
         /// View model object about motor device connected port D of Brick.
         /// </summary>
-        protected Ev3MotorDeviceViewModel _MotorViewModelD;
         public Ev3MotorDeviceViewModel MotorViewModelD
         {
             get
             {
-                if (null == this._MotorViewModelD)
+                if (null == this._MotorViewModelArray[3])
                 {
-                    this._MotorViewModelD = new Ev3MotorDeviceViewModel();
+                    this._MotorViewModelArray[3] = new Ev3MotorDeviceViewModel();
                 }
-                return this._MotorViewModelD;
+                return this._MotorViewModelArray[3];
             }
             set
             {
-                this._MotorViewModelD = value;
+                this._MotorViewModelArray[3] = value;
                 this.RaisePropertyChanged("MotorViewModelD");
+            }
+        }
+
+        /// <summary>
+        /// Array list of Ev3MotorDeviceViewModel.
+        /// </summary>
+        protected Ev3MotorDeviceViewModel[] _MotorViewModelArray;
+        public Ev3MotorDeviceViewModel[] MotorViewModelArray
+        {
+            get
+            {
+                if (null == this._MotorViewModelArray)
+                {
+                    this._MotorViewModelArray = new Ev3MotorDeviceViewModel[4];
+                }
+                return this._MotorViewModelArray;
             }
         }
 
         /// <summary>
         /// View model obejct about sensor device connected port 1 of Brick.
         /// </summary>
-        protected Ev3SensorDeviceViewModel _SensorViewModel1;
         public Ev3SensorDeviceViewModel SensorViewModel1
         {
             get
             {
-                if (null == this._SensorViewModel1)
+                if (null == this._SensorViewModelArray[0])
                 {
-                    this._SensorViewModel1 = new Ev3SensorDeviceViewModel();
+                    this._SensorViewModelArray[0] = new Ev3SensorDeviceViewModel();
                 }
-                return this._SensorViewModel1;
+                return this._SensorViewModelArray[0];
             }
             set
             {
-                this._SensorViewModel1 = value;
+                this._SensorViewModelArray[0] = value;
                 this.RaisePropertyChanged("SensorViewModel1");
             }
         }
@@ -146,20 +166,19 @@ namespace Ev3Controller.ViewModel
         /// <summary>
         /// View model obejct about sensor device connected port 2 of Brick.
         /// </summary>
-        protected Ev3SensorDeviceViewModel _SensorViewModel2;
         public Ev3SensorDeviceViewModel SensorViewModel2
         {
             get
             {
-                if (null == this._SensorViewModel2)
+                if (null == this._SensorViewModelArray[1])
                 {
-                    this._SensorViewModel2 = new Ev3SensorDeviceViewModel();
+                    this._SensorViewModelArray[1] = new Ev3SensorDeviceViewModel();
                 }
-                return this._SensorViewModel2;
+                return this._SensorViewModelArray[1];
             }
             set
             {
-                this._SensorViewModel2 = value;
+                this._SensorViewModelArray[1] = value;
                 this.RaisePropertyChanged("SensorViewModel2");
             }
         }
@@ -167,20 +186,19 @@ namespace Ev3Controller.ViewModel
         /// <summary>
         /// View model obejct about sensor device connected port 3 of Brick.
         /// </summary>
-        protected Ev3SensorDeviceViewModel _SensorViewModel3;
         public Ev3SensorDeviceViewModel SensorViewModel3
         {
             get
             {
-                if (null == this._SensorViewModel3)
+                if (null == this._SensorViewModelArray[2])
                 {
-                    this._SensorViewModel3 = new Ev3SensorDeviceViewModel();
+                    this._SensorViewModelArray[2] = new Ev3SensorDeviceViewModel();
                 }
-                return this._SensorViewModel3;
+                return this._SensorViewModelArray[2];
             }
             set
             {
-                this._SensorViewModel3 = value;
+                this._SensorViewModelArray[2] = value;
                 this.RaisePropertyChanged("SensorViewModel3");
             }
         }
@@ -188,29 +206,78 @@ namespace Ev3Controller.ViewModel
         /// <summary>
         /// View model obejct about sensor device connected port 4 of Brick.
         /// </summary>
-        protected Ev3SensorDeviceViewModel _SensorViewModel4;
         public Ev3SensorDeviceViewModel SensorViewModel4
         {
             get
             {
-                if (null == this._SensorViewModel4)
+                if (null == this._SensorViewModelArray[3])
                 {
-                    this._SensorViewModel4 = new Ev3SensorDeviceViewModel();
+                    this._SensorViewModelArray[3] = new Ev3SensorDeviceViewModel();
                 }
-                return this._SensorViewModel4;
+                return this._SensorViewModelArray[3];
             }
             set
             {
-                this._SensorViewModel4 = value;
+                this._SensorViewModelArray[3] = value;
                 this.RaisePropertyChanged("SensorViewModel4");
             }
         }
+
+        protected Ev3SensorDeviceViewModel[] _SensorViewModelArray;
+        public Ev3SensorDeviceViewModel[] SensorViewModelArray
+        {
+            get
+            {
+                if (null == this._SensorViewModelArray)
+                {
+                    this._SensorViewModelArray = new Ev3SensorDeviceViewModel[4];
+                }
+                return this._SensorViewModelArray;
+            }
+        }
+
+        public Timer UpdateTimer { get; protected set; }
         #endregion
 
         #region Other methods and private properties in calling order
         public void ConnectStateChangedCallback(object sender, EventArgs e)
         {
             Console.WriteLine("Connection State chagned.");
+
+            if (e is ConnectStateChangedEventArgs)
+            {
+                var Arg = e as ConnectStateChangedEventArgs;
+                if (Arg.NewValue.State.Equals(ConnectionState.Connected))
+                {
+                    this.UpdateTimer = new Timer(10);//Interval = 10 msec
+                    this.UpdateTimer.Elapsed += (send_source, arg_data) =>
+                    {
+                        try
+                        {
+                            this.UpdateTimer.Stop();
+                            //Write code here to set Brick data into property.
+                        }
+                        finally
+                        {
+                            this.UpdateTimer.Start();
+                        }
+                    };
+                    this.UpdateTimer.Start();
+                }
+                else
+                {
+                    try
+                    {
+                        this.UpdateTimer.Stop();
+                        this.UpdateTimer = null;
+                    }
+                    catch (NullReferenceException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        Console.WriteLine("Timer has not been started.");
+                    }
+                }
+            }
         }
         #endregion
     }
