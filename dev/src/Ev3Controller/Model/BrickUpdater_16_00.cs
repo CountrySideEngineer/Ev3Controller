778d73f347c316899959edc5133fb2cf52402176
DeepSleepEngineer <deep-sleeper@nify.com>
2017-11-17 18:51:07 +0900

    Add steering handling code.

---
+++ dev/src/Ev3Controller/Model/BrickUpdater_16_00.cs
@@ -0,0 +1,17 @@
+﻿using Ev3Controller.Ev3Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev3Controller.Model
+{
+    public class BrickUpdater_16_00 : BrickUpdater
+    {
+        public override void Update(ACommand Command, Ev3Brick Brick)
+        {
+            //Nothing to update in this function.
+        }
+    }
+}
